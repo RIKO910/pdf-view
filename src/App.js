16ba6df39fa2a469804234213f8f2ecfe81c6cf5
RIKO910 <tarikulislamriko910@gmail.com>
2023-01-04 18:47:49 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
+import WebViewer from '@pdftron/webviewer';
 import './App.css';
 
-function App() {
+const App = () => {
+  const viewer = useRef(null);
+
+  // if using a class, equivalent of componentDidMount 
+  useEffect(() => {
+    WebViewer(
+      {
+        path: '/webviewer/lib',
+        initialDoc: '/files/PDFTRON_about.pdf',
+      },
+      viewer.current,
+    ).then((instance) => {
+      const { documentViewer, annotationManager, Annotations } = instance.Core;
+
+      documentViewer.addEventListener('documentLoaded', () => {
+        const rectangleAnnot = new Annotations.RectangleAnnotation({
+          PageNumber: 1,
+          // values are in page coordinates with (0, 0) in the top left
+          X: 100,
+          Y: 150,
+          Width: 200,
+          Height: 500,
+          Author: annotationManager.getCurrentUser()
+        });
+
+        annotationManager.addAnnotation(rectangleAnnot);
+        // need to draw the annotation otherwise it won't show up until the page is refreshed
+        annotationManager.redrawAnnotation(rectangleAnnot);
+      });
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">React sample</div>
+      <div className="webviewer" ref={viewer}></div>
     </div>
   );
-}
+};
 
 export default App;
